bfc7e191c59be57f5e5fc97611044b9ef25c4948
AAsriyan <aasriyan@protonmail.com>
2019-06-06 16:25:37 -0400

    Day 1 MVP Problem Set

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,33 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
 
+def rock_paper_scissors(numOfRounds):
+    options = ['rock', 'paper', 'scissors']
+    allPossibilities = []
+
+    if numOfRounds == 0:
+        return [allPossibilities]
+
+    def roundChoice(round, roundNum):
+        for i in range(len(options)):
+            round.append(options[i])
+            if roundNum == numOfRounds:
+                allPossibilities.append(round[:])
+            else:
+                roundChoice(round, roundNum + 1)
+            round.pop()
+
+    roundChoice([], 1)
+    print(allPossibilities)
+    return allPossibilities
+
+
+# rock_paper_scissors(2)
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
